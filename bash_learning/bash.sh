2d2f8388b0b0b8375e61b06e254f619a74636aee
Maxim Tretyakov <tretyakov541@mail.ru>
2021-08-22 11:04:51 +0700

    Learning branches

--- bash_learning/bash.sh
+++ bash_learning/bash.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# echo "Hello mudak"
-
-# path="/home/maxim/devops/"
-
-# for list in $(ls $path)
-# do
-# echo "$list"
-# done
-
-for (( i = 0; i < 15; i++ )); do
-  if [[ $i -gt 5 ]] && [[ $i -lt 10 ]]; then
+for (( i = 0; i < 15; i++ ));
+do
+  if [[ $i -gt 5 ]] && [[ $i -lt 10 ]];
+  then
     continue
   fi
   echo "i=$i+1"
