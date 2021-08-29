f86d1ad0f1b3e2f5445224292a6e07944aafd870
Maxim Tretyakov <tretyakov541@mail.ru>
2021-08-29 22:42:41 +0700

    ansible 2

--- bash_learning/bash.sh
+++ bash_learning/bash.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-array=(1 2 3)
+path=$(echo $PATH | sed 's/:/ /g')
+i=0
 
-
-echo ${array1[*]}
+for dir in $path; do
+  list=$(ls $dir)
+  for count in $list; do
+    i=$(($i+1))
+  done
+  echo "$dir --- $i files"
+  i=0
+done
