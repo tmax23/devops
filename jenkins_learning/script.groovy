ba5cf25e76501204bc4b2b2536f2e4bf7fb2f04f
Maxim Tretyakov <tretyakov541@mail.ru>
2021-10-19 22:17:05 +0700

    Modifed Jenkinsfile

---
+++ jenkins_learning/script.groovy
@@ -0,0 +1,14 @@
+def buildApp () {
+    echo "Building app ..."
+}
+
+def testApp () {
+    echo "Testing app ..."
+}
+
+def deployApp () {
+  echo "Deploying the application..."
+  echo "Deploying, print from parameters ${params.VERSION}"
+}
+
+return this
