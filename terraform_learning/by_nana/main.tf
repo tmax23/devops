b0f598f7cbc5b6a54c5288071a5fb705d751de85
Maxim Tretyakov <tretyakov541@mail.ru>
2021-11-01 22:13:45 +0700

    Terraform

--- terraform_learning/by_nana/main.tf
+++ terraform_learning/by_nana/main.tf
@@ -4,8 +4,17 @@ provider "aws" {
   region     = "us-east-2"
 }
 
-resource "aws_instance" "ubuntu_from_terraform" {
-  count         = 2
+# resource "aws_instance" "ubuntu_from_terraform" {
+#   count         = 1
+#   ami           = "ami-00399ec92321828f5"
+#   instance_type = "t2.micro"
+# }
+
+resource "aws_instance" "test" {
   ami           = "ami-00399ec92321828f5"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
