caec0226825777eb8b9feec18834f340d5ebd27c
Maxim Tretyakov <tretyakov541@mail.ru>
2021-11-03 17:13:03 +0700

    Terraform from Work

--- terraform_learning/aws_ec2_create/main.tf
+++ terraform_learning/aws_ec2_create/main.tf
@@ -1,3 +1,104 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.my_region
+}
+
+variable "my_region" {}
+variable "avail_zone" {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "env_prefix" {}
+variable "my_whitelist" {
+  type = list(string)
+}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name = "${var.env_prefix}-rtb"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.my_whitelist
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
+  }
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+/*output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+}*/
+
+resource "aws_instance" "myapp-server" {
+  ami           = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "${var.env_prefix}-myapp-server"
+  }
 }
---
+++ terraform_learning/aws_ec2_create/terraform.tfvars
@@ -0,0 +1,6 @@
+my_region         = "us-east-2"
+avail_zone        = "us-east-2a"
+vpc_cidr_block    = "10.0.0.0/16"
+subnet_cidr_block = "10.0.1.0/24"
+env_prefix        = "dev"
+my_whitelist      = ["95.167.217.2/32", "8.8.8.8/32"]
