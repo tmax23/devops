053cebf3f2ec705d60e676767ffd6e0e4d53f38c
Maxim Tretyakov <tretyakov541@mail.ru>
2021-11-08 16:32:13 +0700

    Terraform from Work - Jenkins

--- terraform_learning/aws_jenkins/server-cmds.sh
+++ terraform_learning/aws_jenkins/server-cmds.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export IMAGE=$1
-docker-compose down
+docker-compose -f docker-compose.yaml down
+docker pull $1
 docker-compose -f docker-compose.yaml up --detach
 echo "success"
